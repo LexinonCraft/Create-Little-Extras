f54ce5ac5d0998d01117bc791b0ab0467a46750b
LexinonCraft <willi4wuff@gmail.com>
2022-07-27 17:22:56 +0200

    *Switched ;)

--- src/main/java/com/lexinonc/createLe/CreateLittleExtras.java
+++ src/main/java/com/lexinonc/createLe/CreateLittleExtras.java
@@ -1,21 +1,16 @@
 package com.lexinonc.createLe;
 
-import com.mojang.logging.LogUtils;
 import com.simibubi.create.foundation.block.BlockStressValues;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.repack.registrate.util.nullness.NonNullSupplier;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
 
 @Mod(CreateLittleExtras.MODID)
 public class CreateLittleExtras {
 
     public static final String MODID = "create_le";
-
-    // Directly reference a slf4j logger
-    public static final Logger LOGGER = LogUtils.getLogger();
 
     private static final NonNullSupplier<CreateRegistrate> REGISTRATE = CreateRegistrate.lazy(MODID);
 
